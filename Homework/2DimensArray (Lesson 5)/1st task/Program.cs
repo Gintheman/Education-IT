b7c4db80be940d90c87081affdeb49c4f364dbb4
Eugene <joker3lolz@gmail.com>
2023-12-22 15:44:14 +0300

    1st task Lesson 5 is done.

--- Homework/2DimensArray (Lesson 5)/1st task/Program.cs
+++ Homework/2DimensArray (Lesson 5)/1st task/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+int [,] array = new int [5,3];
+
+void CreateArray()
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray() 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void FindElement ()
+{
+    
+
+    Console.WriteLine($"Enter the 1st index: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Enter the 2nd index: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+    if(num1 < array.GetLength(0) && num2 < array.GetLength(1))
+    {
+        
+        System.Console.WriteLine($"{array[num1, num2]}");
+        
+    }  
+    else
+    {
+        System.Console.WriteLine($"No such element. Try again");
+    }
+    
+}
+
+
+CreateArray();
+PrintArray();
+FindElement();
+
