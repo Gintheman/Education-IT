dc2bfd26401186537aff0c85b2a3fb68a90d8f87
Eugene <joker3lolz@gmail.com>
2023-12-27 14:35:29 +0300

    3d task is done but may have some questions.

--- Homework/2DimensArray (Lesson 5)/3d task/Program.cs
+++ Homework/2DimensArray (Lesson 5)/3d task/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [,] array = new int [3,5];
+
+void CreateArray()
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray() 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void FindMinSum()
+{
+    int sumElement = 0;
+    int min = 100;
+    int index = 0;
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sumElement += array[i, j];
+            
+            
+        }
+        System.Console.WriteLine($"{sumElement}");
+        
+        if(min > sumElement)
+        {   
+            min = sumElement;
+            index = i;
+        }
+
+        
+       
+        
+        
+    }
+    System.Console.WriteLine($"Минимальная сумма в строке {index}");
+    
+
+
+
+
+
+
+
+}
+
+CreateArray();
+PrintArray();
+FindMinSum();
